993062366fe814c380b514c6314474281cb68b5e
JatinSesma <gholujatt0@gmail.com>
2024-05-17 10:47:22 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+let navicon = document.querySelector(".nav-icon");
+let lgview = document.querySelector(".lg-view");
+navicon.addEventListener("click", function () {
+    lgview.classList.toggle("right-0");
+})
