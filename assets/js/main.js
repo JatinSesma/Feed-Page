ae9ab4dcaeddda6d35f8307e3eeeb8602db94679
JatinSesma <gholujatt0@gmail.com>
2024-05-23 12:34:55 +0530

    responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let navicon = document.querySelector(".nav-icon");
-let lgview = document.querySelector(".lg-view");
+let lgview = document.querySelector(".menulist");
 navicon.addEventListener("click", function () {
-    lgview.classList.toggle("right-0");
+    lgview.classList.toggle("");
 })
